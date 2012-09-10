bb30e49398311bf973c1312204e29a7d3cea15ba
Roy van Baekel <roy@byte.nl>
2012-09-10 10:50:04 +0000

    Fixed missing imports

--- create-apachestatus-alarm.py
+++ create-apachestatus-alarm.py
@@ -4,13 +4,15 @@ import commands
 import boto, boto.ec2, boto.ec2.cloudwatch
 import metrics,time 
 
+from boto.ec2.cloudwatch import MetricAlarm
+
 thisInstanceId      = commands.getoutput("wget -q -O - http://169.254.169.254/latest/meta-data/instance-id")
 thisRegion          = commands.getoutput("wget -q -O - http://169.254.169.254/latest/meta-data/placement/availability-zone")
 namespace           = "Byte/System"
 conn                = boto.ec2.cloudwatch.connect_to_region(thisRegion[:-1])
 alarmname           = thisInstanceId + "ApacheStatus"
 metricname          = "Apachestatus"
-unitname            = "Count"		
+unitname            = "Average"	
 apachemetrics       = metrics.apacheMetrics()
 dimensions          = {"instanceId" : thisInstanceId}
 
@@ -24,7 +26,5 @@ ApacheStatusAlarm   = MetricAlarm(name=alarmname,
                                     threshold='2',
                                     period='60',
                                     evaluation_periods=2,
-                                    actions_enabled=True,
-                                    alarm_action=alarm_actions,
                                     dimensions=dimensions)
 conn.create_alarm(ApacheStatusAlarm)
